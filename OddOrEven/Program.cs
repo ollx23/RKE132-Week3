d384b3406d32d33efc24fa269ff8cfb51aa50949
Balja. <gohot.poolggmail.com.>
2024-04-26 00:56:51 +0300

    add week 3 code

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+Console.WriteLine("Enter a number:");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+int result = userNum % 2;
+
+if (result != 0)
+{
+    Console.WriteLine("User number is odd.");
+}
+else
+{
+    Console.WriteLine("User number is even.");
+}
